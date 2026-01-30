7e4a3753a4d43d8f567f05b0b1d49d95dcffd25f
ahmedkhan753 <ahmedk32410@gmail.com>
2026-01-30 21:24:12 +0500

    added a raw data fetcher method

---
+++ data_transform/raw_data_fetcher.py
@@ -0,0 +1,8 @@
+import logging
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
+
+database_url = "postgresql://admin:password123@postgres:5432/bi_warehouse"
+
+def raw_data_fetcher():
