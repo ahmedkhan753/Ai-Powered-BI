83adc30fd0ecd44bb59b2215882e1e1ca4927708
ahmedkhan753 <ahmedk32410@gmail.com>
2026-02-02 17:42:02 +0500

    populated facts

--- data_warehouse/star_schema.py
+++ data_warehouse/star_schema.py
@@ -1,6 +1,5 @@
 # We Are going to build the Gold layer which will take the data from silver layer and then distribute it into facts and dimensions for performance, BI dashboards and queries
 
-
 # 1. Import necessary libraries
 import pandas as pd
 import numpy as np
@@ -18,8 +17,9 @@ silver_data = load_from_silver()
 
 # 5. Define the star schema
 class StarSchema:
-    def __init__(self, engine):
+    def __init__(self, engine, silver_data):
         self.engine = engine
+        self.silver_data = silver_data
 
     def build_star_schema(self):
         # Build the star schema
@@ -27,21 +27,22 @@ class StarSchema:
         self.build_dimension_tables()
 
     def build_fact_table(self):
-        # Build the fact table
-        pass
+        # Here we will populate the fact table
+        gold_fact_sales = pd.read_sql("INSERT INTO warehouse.fact_sales(sales_key,order_id,date_key,customer_key,product_key,quantity,price,sales_amount,ingestion_timestamp)",self.engine_gold,con=)
+
 
     def build_dimension_tables(self):
         # Build the dimension tables
         pass
 
 # 6. Create an instance of the star schema
-star_schema = StarSchema(engine)
+star_schema = StarSchema(engine_gold)
 
 # 7. Build the star schema
 star_schema.build_star_schema()
 
 # 8. Close the database connection
-engine.dispose()
+engine_gold.dispose()
 
 # 9. Print a success message
 print("Star schema built successfully!")
