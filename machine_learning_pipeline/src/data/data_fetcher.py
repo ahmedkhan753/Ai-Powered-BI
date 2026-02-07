d9802de172547c7404acdbd6e6f66dcd189a9ab7
ahmedkhan753 <ahmedk32410@gmail.com>
2026-02-07 22:56:35 +0500

    added data reader

--- machine_learning_pipeline/src/data/data_fetcher.py
+++ machine_learning_pipeline/src/data/data_fetcher.py
@@ -21,7 +21,7 @@ def fetch_data(config):
     """
     try:
         logger.info("Fetching data from Gold Layer...")
-        
+        df = pd.read_sql(config['database']['query'], engine_gold)
         return df
         
     except Exception as e:
